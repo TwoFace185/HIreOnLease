9447546b6eb9dc13b6a7bc69234d79232f86036e
bhupesh-smartgeeks <bhupesh.nikhurpa@smartgeeks.io>
2024-10-27 09:12:21 +0530

    Initial Commmit

---
+++ src/main/java/com/Bonami/HireOnlease/HireOnleaseApplication.java
@@ -0,0 +1,13 @@
+package com.Bonami.HireOnlease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HireOnleaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HireOnleaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Bonami/HireOnlease/HireOnleaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Bonami.HireOnlease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HireOnleaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
